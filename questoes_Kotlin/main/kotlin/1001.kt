89ca1bd7dd51075c77157447396969c96d230d4d
Camila Claudino <80794452+Erundule@users.noreply.github.com>
2022-08-07 22:53:10 -0300

    Update 1001.kt

--- questoes_Kotlin/main/kotlin/1001.kt
+++ questoes_Kotlin/main/kotlin/1001.kt
@@ -1,7 +1,8 @@
-val a: Int = readLine()!!.toInt()
-val b: Int = readLine()!!.toInt()
-val soma: Int = a + b
-println("X = $soma")
-
+fun main(){
+   val a: Int = readLine()!!.toInt()
+   val b: Int = readLine()!!.toInt()
+   val soma: Int = a + b
+   println("X = $soma")
+}
 
    
