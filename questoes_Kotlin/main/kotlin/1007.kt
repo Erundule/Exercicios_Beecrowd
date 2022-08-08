e5e60617c46f1713abcc3900b8d6c72b2125cc52
Camila Claudino <80794452+Erundule@users.noreply.github.com>
2022-08-07 22:54:27 -0300

    Update 1007.kt

--- questoes_Kotlin/main/kotlin/1007.kt
+++ questoes_Kotlin/main/kotlin/1007.kt
@@ -1,4 +1,4 @@
-fun main10092889() {
+fun main() {
     val a: Int = readLine()!!.toInt()
     val b: Int = readLine()!!.toInt()
     val c: Int = readLine()!!.toInt()
@@ -7,19 +7,3 @@ fun main10092889() {
     println("DIFERENCA = $difference")
 }
 
-fun main1006() {
-    val a: Double = readLine()!!.toDouble()
-    val b: Double = readLine()!!.toDouble()
-    val c: Double = readLine()!!.toDouble()
-    val media = (a * 2 + b * 3 + c * 5) / 10
-    val m = ("%.1f".format(media))
-    println("MEDIA = $m")
-}
-fun main() {
-    val time: Int = readLine()!!.toInt()
-    val speed: Int = readLine()!!.toInt()
-    val averageSpeed = time * speed
-    val media = averageSpeed.toDouble()
-    val fuel: Double = media/ 12
-    println(String.format("%.3f", fuel))
-}
